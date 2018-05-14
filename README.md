8d1ec538bc35a845ebbbb5d39d2d888c5e2b8937
achilles_xushy <yuqingxushiyin@gmail.com>
2018-05-14 21:44:39 +0800

    1 基础结构提交

---
+++ README.md
@@ -0,0 +1,6 @@
+# 微信公众号开发
+    * author：achilles_xushy
+    * date: 2018-05-14
+
+## 2018-05-14
+    1. 上传github用于部署服务器
---
+++ setting.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@license : (C) Copyright 2013-2018, Easy doesnt enter into grown-up life.
+@Software: PyCharm
+@Project : wechatInfo
+@Time : 2018/5/14 下午9:33
+@Author : achilles_xushy
+@contact : yuqingxushiyin@gmail.com
+@Site : 
+@File : setting.py
+@desc :
+"""
+
+if __name__ == '__main__':
+    pass
